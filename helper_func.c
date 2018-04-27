727e101e12e08c423ed4b6432f272f8ff6a418bf
Isaac Wong <isaac@thirdcaptain.com>
2018-04-27 06:22:23 +0000

    End of project commit

--- helper_func.c
+++ helper_func.c
@@ -24,8 +24,6 @@ int is_number(char *string)
 		i++;
 	}
 
-/*	printf("num_digits: %d\n", num_digits);
-	printf("length: %d\n", length);*/
 	if (num_digits == length)
 		return (0);
 
@@ -47,36 +45,3 @@ void remove_newline(char *string)
 			string[i] = '\0';
 	}
 }
-
-/*
-int main(void)
-{
-	char *s1 = "q14";
-	char *s2 = "-123ddc";
-	char *s3 = "123999";
-	int value = 0;
-
-	printf("s1: %d\n", atoi(s1));
-	value = is_number(s1);
-	if (value == 0)
-		printf("%s is a number\n", s1);
-	else
-		printf("%s is not a number\n", s1);
-
-	printf("s2: %d\n", atoi(s2));
-	value = is_number(s2);
-	if (value == 0)
-		printf("%s is a number\n", s2);
-	else
-		printf("%s is not a number\n", s2);
-
-	printf("s3: %d\n", atoi(s3));
-	value = is_number(s3);
-	if (value == 0)
-		printf("%s is a number\n", s3);
-	else
-		printf("%s is not a number\n", s3);
-
-	return(0);
-}
-*/
